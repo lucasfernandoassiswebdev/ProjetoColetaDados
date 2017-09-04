e6cb81ffce845b6b730a25fb63df60da7ea6ebbe
Lucas Fernando <lucasaassis@outlook.com>
2017-09-04 13:04:54 -0300

    Finalizando transferência de dados do excel para a base sql

--- Coleta.WebAspNet/Controllers/HomeController.cs
+++ Coleta.WebAspNet/Controllers/HomeController.cs
@@ -37,11 +37,11 @@ namespace Coleta.WebAspNet.Controllers
             package = new ExcelPackage(new FileInfo(filePath));
             workSheet = package.Workbook.Worksheets.First();
             
-            for (int i = workSheet.Dimension.Start.Row + 1; i <= workSheet.Dimension.End.Row; i++)
+            for (int i = workSheet.Dimension.Start.Row + 1; i <= 51; i++)
             {
                 //tratando os dados para pegar as informações das colunas corretas
-                var locomocao = workSheet.Cells[i, 10].ToString() == string.Empty ?  11 :  10;
-                var trabalha = workSheet.Cells[i, 14].ToString() == "Sozinho" ? 15 : 19;
+                var locomocao = workSheet.Cells[i, 10].Value == null ?  11 :  10;
+                var trabalha = workSheet.Cells[i, 14].Value.ToString() == "Sozinho" ? 15 : 19;
                 var periodoEstudo = trabalha == 15 ? 17 : 22;
                 var periodoTrabalho = workSheet.Cells[i, periodoEstudo].ToString() == "Matutino" ? 24 : 23;
 
@@ -64,15 +64,15 @@ namespace Coleta.WebAspNet.Controllers
                     Linguas = workSheet.Cells[i, 29].Value?.ToString() ?? "nenhuma",
                     Trabalha = workSheet.Cells[i, trabalha].Value.ToString(),
                     PeriodoEstudo = workSheet.Cells[i, periodoEstudo].Value.ToString(),
-                    PessoasResidem = workSheet.Cells[i, 18].Value.ToString(),
-                    PessoasTrabalham = workSheet.Cells[i, 20].Value.ToString(),
-                    SomaRendas = workSheet.Cells[i, 21].Value.ToString(),
+                    PessoasResidem = workSheet.Cells[i, 18].Value?.ToString() ?? "0",
+                    PessoasTrabalham = workSheet.Cells[i, 20].Value?.ToString() ?? "1",
+                    SomaRendas = workSheet.Cells[i, 21].Value?.ToString() ?? "0",
                     PeriodoTrabalho = workSheet.Cells[i, periodoTrabalho].Value?.ToString() ?? "não trabalha",
                     VidaEscolar = workSheet.Cells[i, 25].Value.ToString(),
                     ConhecimentoInformatica = workSheet.Cells[i, 26].Value.ToString(),
                     MotivoVestibular = workSheet.Cells[i, 27].Value.ToString(),
                     ConhecimentoLingua = workSheet.Cells[i, 28].Value.ToString(),
-                    Meio = workSheet.Cells[i, 30].Value.ToString()
+                    Meio = workSheet.Cells[i, 30].Value?.ToString() ?? "Web"
                 };
                 
                 _appAlunos.InsereAluno(aluno);
